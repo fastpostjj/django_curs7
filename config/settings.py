3568908957299a6b4e537c48a62fedcf441d253b
fastpostjj <fastpost@yandex.ru>
2023-08-31 00:53:45 +0300

    finished

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,6 @@ SECRET_KEY = \
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-print("DEB=",DEBUG)
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['*']
